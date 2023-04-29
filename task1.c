c732d1f33b9ec9502283151213f731f5a81c0676
Alina Sul <48648714+Alina-sul@users.noreply.github.com>
2023-04-29 13:07:23 +0300

    wip task1

--- task1.c
+++ task1.c
@@ -1,16 +1,51 @@
-/* bn_sample.c */
+/* task1.c */
 #include <stdio.h>
 #include <openssl/bn.h>
 #define NBITS 256
 
 void printBN(char *msg, BIGNUM * a)
 {
-  /* Use BN_bn2hex(a) for hex string
-   * Use BN_bn2dec(a) for decimal string */
   char * number_str = BN_bn2hex(a);
   printf("%s %s\n", msg, number_str);
   OPENSSL_free(number_str);
 }
+
+// Extended Euclidean Algorithm
+int BN_mod_inverse_extended_euclidean(BIGNUM *r, const BIGNUM *a, const BIGNUM *m, BN_CTX *ctx) {
+    BIGNUM *t = BN_new();
+    BIGNUM *nt = BN_new();
+    BIGNUM *r_next = BN_new();
+    BIGNUM *m_copy = BN_new();
+
+    BN_copy(m_copy, m);
+
+    BN_one(r);
+    BN_zero(t);
+
+    while (BN_cmp(a, BN_value_one()) > 0) {
+        BN_mod(r_next, a, m, ctx);
+        BN_div(nt, NULL, m, a, ctx);
+
+        BIGNUM *tmp = BN_new();
+        BN_mul(tmp, nt, r, ctx);
+        BN_mod_sub(t, t, tmp, m_copy, ctx);
+
+        BN_swap(a, r_next);
+        BN_swap(r, t);
+    }
+
+    if (BN_is_negative(r)) {
+        BN_add(r, r, m_copy);
+    }
+
+    BN_free(t);
+    BN_free(nt);
+    BN_free(r_next);
+    BN_free(m_copy);
+
+    return 1;
+}
+
 int main ()
 {
   BN_CTX *ctx = BN_CTX_new();
@@ -19,24 +54,28 @@ int main ()
   BIGNUM *e = BN_new();
   BIGNUM *n = BN_new();
   BIGNUM *etf_of_n = BN_new();
-  BIGNUM *res = BN_new();
+  BIGNUM *d = BN_new();
   BIGNUM *p_minus1 = BN_new();
   BIGNUM *q_minus1 = BN_new();
   
-  // Initialize a, b, n
+  // Initialize p, q, e
   BN_hex2bn(&p, "F7E75FDC469067FFDC4E847C51F452DF");
   BN_hex2bn(&q, "E85CED54AF57E53E092113E62F436F4F");
   BN_hex2bn(&e, "0D88C3");
 
-  // Calculate n
+  // Calculate n = p * q
   BN_mul(n, p, q, ctx);
   printBN("n = ", n);
   
-  // Calculate Euler's totient function of n
+  // Calculate Euler's totient function of n: φ(n) = (p-1) * (q-1)
   BN_sub(p_minus1, p, BN_value_one());
   BN_sub(q_minus1, q, BN_value_one());
   BN_mul(etf_of_n, p_minus1, q_minus1, ctx);
   printBN("φ(n) = ", etf_of_n);
+
+  // Calculate the modular multiplicative inverse of e modulo φ(n): d
+  BN_mod_inverse_extended_euclidean(d, e, etf_of_n, ctx);
+  printBN("d = ", d);
 
   // Free the allocated memory
   BN_free(p);
@@ -44,7 +83,7 @@ int main ()
   BN_free(e);
   BN_free(n);
   BN_free(etf_of_n);
-  BN_free(res);
+  BN_free(d);
   BN_free(p_minus1);
   BN_free(q_minus1);
   BN_CTX_free(ctx);
