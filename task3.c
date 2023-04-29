c797a909b5d2f94741feaccfe1be43ddf3674104
Alina Sul <48648714+Alina-sul@users.noreply.github.com>
2023-04-29 20:00:05 +0300

    wip task3

--- task3.c
+++ task3.c
@@ -7,7 +7,7 @@ void decrypt(BIGNUM *C, BIGNUM *d, BIGNUM *n, char **result_str)
   BN_CTX *ctx = BN_CTX_new();
   BIGNUM *M = BN_new();
 
-  // Decrypt the ciphertext using the private key (d, n)
+  // Decrypt the message using the private key (d, n)
   BN_mod_exp(M, C, d, n, ctx);
 
   // Convert the decrypted message to a hexadecimal string
@@ -34,25 +34,40 @@ int main()
   BIGNUM *n = BN_new();
   BIGNUM *e = BN_new();
   BIGNUM *d = BN_new();
+  BIGNUM *M = BN_new();
   BIGNUM *C = BN_new();
   BN_CTX *ctx = BN_CTX_new();
 
-  // Initialize n, e, d to the given values from Task 2
+  // Initialize n, e, d to the given values
   BN_hex2bn(&n, "DCBFFE3E51F62E09CE7032E2677A78946A849DC4CDDE3A4D0CB81629242FB1A5");
   BN_hex2bn(&e, "010001");
   BN_hex2bn(&d, "74D806F9F3A62BAE331FFE3F0A68AFE35B3D2E4794148AACBC26AA381CD7D30D");
 
-  // Initialize the ciphertext C to the given value
-  BN_hex2bn(&C, "3ADF3EEF3C8E2BFB9C24B2EDF77B1F8B2FC4E4F4C4DCDBB0B8C0E9B9B180B1D4");
+  // Convert the ciphertext to a BIGNUM
+  BN_hex2bn(&C, "3BDA");
 
-  // Decrypt the ciphertext using the private key (d, n)
+  // Decrypt the message using the private key (d, n)
   char *result_str;
   decrypt(C, d, n, &result_str);
+
+  // Convert the result to a Caesar cipher and try all possible keys
+  for (int i = 0; i < 26; i++) {
+    char *message_str = (char*) malloc(strlen(result_str) + 1);
+    int j;
+    for (j = 0; j < strlen(result_str); j++) {
+      message_str[j] = 'A' + ((result_str[j] - 'A' - i + 26) % 26);
+    }
+    message_str[j] = '\0';
+
+    printf("Possible decryption with key %d: %s\n", i, message_str);
+    free(message_str);
+  }
 
   // Free memory
   BN_free(n);
   BN_free(e);
   BN_free(d);
+  BN_free(M);
   BN_free(C);
   return 0;
 }
