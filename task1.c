db9a06a65a990fc508d74c1f394f26b8233d2f42
Alina Sul <48648714+Alina-sul@users.noreply.github.com>
2023-04-29 10:06:45 +0300

    wip task 1

---
+++ task1.c
@@ -0,0 +1,34 @@
+/* bn_sample.c */
+#include <stdio.h>
+#include <openssl/bn.h>
+#define NBITS 256
+
+void printBN(char *msg, BIGNUM * a)
+{
+  /* Use BN_bn2hex(a) for hex string
+   * Use BN_bn2dec(a) for decimal string */
+  char * number_str = BN_bn2hex(a);
+  printf("%s %s\n", msg, number_str);
+  OPENSSL_free(number_str);
+}
+int main ()
+{
+  BN_CTX *ctx = BN_CTX_new();
+  BIGNUM *p = BN_new();
+  BIGNUM *q = BN_new();
+  BIGNUM *e = BN_new();
+  BIGNUM *n = BN_new();
+  BIGNUM *res = BN_new();
+  
+  // Initialize a, b, n
+  BN_hex2bn(&p, "F7E75FDC469067FFDC4E847C51F452DF");
+  BN_hex2bn(&q, "E85CED54AF57E53E092113E62F436F4F");
+  BN_hex2bn(&e, "0D88C3");
+  
+  // Calculate n
+  BN_mul(res, p, q, ctx);
+  printBN("n = ", res);
+  
+
+  return 0;
+}
